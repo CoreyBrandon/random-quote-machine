1e255437442e248d7b5238a26e8da933376b7d16
Brandon C <63110859+cbshipt@users.noreply.github.com>
2020-12-06 03:09:21 -0600

    Add random quote machine

---
+++ quotes.js
@@ -0,0 +1,10 @@
+const QUOTEBANK = [{
+            "quote":"I try to work out, but a lot of hotels don't have gyms anymore, so I always try to find a local gym where there's not a ton of 'roid-heads, 'cause I can't stand them. It's tough.",
+        author: "Glenn Danzig"},
+     {
+        "quote":"I think one of the interesting things is that vi is really a mode-based editor.", 
+        author: "Bill Joy"},
+     {
+        "quote":"If you can imagine someone surpassing you, you should do it yourself.",
+        author: "Paul Graham"}
+];
